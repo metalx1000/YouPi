17e158569addb6701497e484ce7ab6b8c286b4c6
metalx1000 <metalx1000@users.noreply.github.com>
2015-01-02 16:21:42 -0500

    Update youplay.sh

--- youplay.sh
+++ youplay.sh
@@ -3,16 +3,15 @@
 id="$1"
 video="/tmp/youtube.mp4"
 
-killall kodi.bin
-killall youtube-dl
-killall omxplayer
+#killall kodi.bin
+ps ux|grep youtube|awk '{print $2}'|while read pid;do kill $pid;done
+#ps ux|grep omxplayer|awk '{print $2}'|while read pid;do kill $pid;done
 
 rm $video*
 youtube-dl "https://www.youtube.com/watch?v=$id" -o $video &
-sleep 8
-omxplayer -o hdmi $video*
+sleep 10
+wget -q -O- "http://localhost:8090/jsonrpc?request={%22jsonrpc%22:%222.0%22,%22method%22:%22Player.Open%22,%22params%22:{%22item%22:{%22file%22:%22$video*%22}},%22id%22:%221%22}"
 
-killall youtube-dl
-rm $video*
+ps ux|grep youtube|awk '{print $2}'|while read pid;do kill $pid;done
 
-/home/pi/.xbmc-current/xbmc-bin/lib/kodi/kodi.bin --standalone -fs
+#/home/pi/.xbmc-current/xbmc-bin/lib/kodi/kodi.bin --standalone -fs
