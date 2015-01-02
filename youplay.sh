be401c0b55fac8435c10e8b9c1b7f7119d149803
metalx1000 <metalx1000@users.noreply.github.com>
2015-01-02 16:48:39 -0500

    Update youplay.sh

--- youplay.sh
+++ youplay.sh
@@ -8,9 +8,9 @@ ps ux|grep youtube|awk '{print $2}'|while read pid;do kill $pid;done
 #ps ux|grep omxplayer|awk '{print $2}'|while read pid;do kill $pid;done
 
 rm $video*
-youtube-dl "https://www.youtube.com/watch?v=$id" -o $video &
+youtube-dl --no-part "https://www.youtube.com/watch?v=$id" -o $video &
 sleep 10
-wget -q -O- "http://localhost:8090/jsonrpc?request={%22jsonrpc%22:%222.0%22,%22method%22:%22Player.Open%22,%22params%22:{%22item%22:{%22file%22:%22$video*%22}},%22id%22:%221%22}"
+wget -q -O- "http://localhost:8090/jsonrpc?request={%22jsonrpc%22:%222.0%22,%22method%22:%22Player.Open%22,%22params%22:{%22item%22:{%22file%22:%22$video%22}},%22id%22:%221%22}"
 
 ps ux|grep youtube|awk '{print $2}'|while read pid;do kill $pid;done
 
