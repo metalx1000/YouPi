7a1c5fedf7e5a7bd0c30eefe2c1f0f8350c132d7
metalx1000 <github@filmsbykris.com>
2015-01-03 10:59:16 -0500

    added update script and video quality option

---
+++ update.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Make sure only root can run our script
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 
+   echo "Trying to restart script as root"
+   sudo $?
+   exit
+fi
+
+busybox="/bin/busybox"
+www="/etc/www"
+cgi="$www/cgi-bin"
+cmd="$busybox httpd -p 8080 -h $www"
+youtubedl="/usr/bin/youtube-dl"
+
+cd $cgi
+rm *
+wget --no-check-certificate "https://raw.githubusercontent.com/metalx1000/YouPi/master/www/cgi-bin/youplayer.cgi"
+chmod +x *.cgi
+
+cd /usr/local/bin/
+rm youplay.sh
+wget --no-check-certificate "https://raw.githubusercontent.com/metalx1000/YouPi/master/youplay.sh"
+chmod +x youplay.sh
+
+$cmd
