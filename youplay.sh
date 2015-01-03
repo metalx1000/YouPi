0e1fc092690e3a6901512d0991e1bd3ada18be2e
metalx1000 <github@filmsbykris.com>
2015-01-03 11:20:05 -0500

    fixed killing youtube-dl too soon

--- youplay.sh
+++ youplay.sh
@@ -13,11 +13,15 @@ fi
 ps ux|grep youtube|awk '{print $2}'|while read pid;do kill $pid;done
 #ps ux|grep omxplayer|awk '{print $2}'|while read pid;do kill $pid;done
 
+echo "<h2>Retrieving Video</h2>"
 rm $video*
 youtube-dl --no-part $q "https://www.youtube.com/watch?v=$id" -o $video &
 sleep 10
-wget -q -O- "http://localhost:8090/jsonrpc?request={%22jsonrpc%22:%222.0%22,%22method%22:%22Player.Open%22,%22params%22:{%22item%22:{%22file%22:%22$video%22}},%22id%22:%221%22}"
+echo "<h2>Loading Video</h2>"
+#sleep 5
 
-ps ux|grep youtube|awk '{print $2}'|while read pid;do kill $pid;done
+wget -q -O- "http://localhost:8090/jsonrpc?request={%22jsonrpc%22:%222.0%22,%22method%22:%22Player.Open%22,%22params%22:{%22item%22:{%22file%22:%22$video%22}},%22id%22:%221%22}"
+echo "<h2>Playing Video</h2>"
+#ps ux|grep youtube|awk '{print $2}'|while read pid;do kill $pid;done
 
 #/home/pi/.xbmc-current/xbmc-bin/lib/kodi/kodi.bin --standalone -fs
